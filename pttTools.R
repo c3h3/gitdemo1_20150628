fb8037bedc6bfb596acf0b421c5c5371834b3dc7
JulieYeh <bigruler@gmail.com>
2015-06-28 12:16:17 +0800

    ptt julie

---
+++ pttTools.R
@@ -0,0 +1,23 @@
+
+library(httr)
+
+
+####################################################
+# getPostUrls via GET Method with cookie
+####################################################
+getPostUrls<-function(x){
+listPageUrl = allListUrls[10]
+res <- GET(listPageUrl,set_cookies(over18=1))
+node = content(res, encoding = "utf8")
+node[cssToXpath(".title a")]
+postUrls = cssApply(node,".title a",function(node){
+  sprintf("https://www.ptt.cc%s",xmlAttrs(node)["href"])
+})
+}
+
+
+resWithoutCookie <-"https://www.ptt.cc/bbs/Gossiping/index.html"
+getPostUrls(resWithoutCookie)
+
+
+
